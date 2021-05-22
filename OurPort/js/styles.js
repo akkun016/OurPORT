c0165fbc45a8784e22a464cf4aceaca42f9e51e8
akkun016 <nakataakito@icloud.com>
2021-05-22 21:32:13 +0900

    contents部分のアニメーション実装・headerのリニューアル

--- OurPort/js/styles.js
+++ OurPort/js/styles.js
@@ -1,6 +1,24 @@
 $(function(){
-  // リロード時スクロール位置をトップへ
-  $('html,body').animate({ scrollTop: 0 }, '1');
+  // マウスカーソルのエフェクト
+  let cursor = $(".cursor");
+  $(document).on("mousemove", function(e) {
+    let mousemove_left = e.screenX - 8;
+    let mousemove_top = e.screenY - 120;
+
+    cursor.css({"transform":`translate(${mousemove_left + "px"}, ${mousemove_top + "px"})`, "opacity": "1"});
+  });
+
+  $("a").hover(function(){
+    cursor.css({"height":"80px", "width":"80px", "top":"-30px", "left":"-30px"})
+  }, function(){
+    cursor.css({"height":"16px", "width":"16px", "top":"0", "left":"0"})
+  });
+
+  $("label").hover(function(){
+    cursor.css({"height":"80px", "width":"80px", "top":"-30px", "left":"-30px"})
+  }, function(){
+    cursor.css({"height":"16px", "width":"16px", "top":"0", "left":"0"})
+  });
 
   // svgアニメーションの設定
   const anime = () =>
@@ -18,20 +36,25 @@ $(function(){
   }
   anime();
 
-  // ページ表示直後5.5秒はスクロール無効
-  setTimeout(() => {
-    $("body").css("overflow", "scroll");
-  }, 5500);
+  // スクロール時の処理
+  const logo = $(".header-title");
+  const nav = $("nav");
+  const icon = $(".mouth-icon > svg");
 
-  // スクロール時にstickyの追加
+  const contentBlocks = document.querySelectorAll(".content-inner");
+  const contentsHeight = document.querySelectorAll(".content-inner").length * 500 + 1300;
   $(window).on("scroll", function() {
-    const logo = $(".main-visual__inner");
-    logo.toggleClass("sticky", window.scrollY > 0);
-
-    const nav = $("nav");
+    logo.toggleClass("sticky", window.scrollY > 0)
     nav.toggleClass("sticky", window.scrollY > 0);
-
-    const icon = $(".mouth-icon > svg");
     icon.toggleClass("sticky", window.scrollY > 0);
+
+    if (window.scrollY > 1300 && window.scrollY < contentsHeight) {
+      for (let i = 0; i < contentBlocks.length; i++) {
+        const contentPosition = contentBlocks[i].getBoundingClientRect().top;
+        if (window.innerHeight - 300 > contentPosition) {
+          contentBlocks[i].children[0].style.display = 'flex';
+        }
+      }
+    }
   })
 });
